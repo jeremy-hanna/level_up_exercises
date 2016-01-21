741c7ebdf4cf4a6a2e29bb40552b0ee2deea1436
Jeremy Hanna <jeremy@whoisjeremy.me>
2016-01-20 19:42:03 -0600

    refactored robot_name.rb for 03_short_methods

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -1,27 +1,51 @@
-class NameCollisionError < RuntimeError; end
-
 class Robot
   attr_accessor :name
 
-  @@registry
+  @@registry ||= [] 
 
   def initialize(args = {})
-    @@registry ||= []
-    @name_generator = args[:name_generator]
+    naming_method = args[:name_generator] || self.method(:generate_name)
 
-    if @name_generator
-      @name = @name_generator.call
-    else
-      generate_char = -> { ('A'..'Z').to_a.sample }
-      generate_num = -> { rand(10) }
+    @name = naming_method.call
+    add_to_registry
+  end
 
-      @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
+  def generate_name
+    @name = "#{call_multiple(2, self.method(:generate_char))}" \
+            "#{call_multiple(3, self.method(:generate_num))}"
+  end
+
+  def add_to_registry
+    if name_collision? @name
+      raise NameCollisionError, "There was a problem generating the robot name, #{@name}!"
     end
 
-    raise NameCollisionError, 'There was a problem generating the robot name!' if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
     @@registry << @name
   end
+
+  def call_multiple(number, lam)
+    string = ''
+    number.times do |i|
+      string += lam.call
+    end
+
+    string
+  end
+
+  def generate_char 
+    ('A'..'Z').to_a.sample 
+  end
+
+  def generate_num 
+    rand(10).to_s
+  end 
+  
+  def name_collision?(name)
+    !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
+  end
 end
+
+NameCollisionError = Class.new(RuntimeError)
 
 robot = Robot.new
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
